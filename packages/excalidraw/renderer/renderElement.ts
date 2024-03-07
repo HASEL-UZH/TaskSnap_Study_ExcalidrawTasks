a543b54921f544b219efc1c369971a56ac75c70a
JulianaGSouza <juulianags@gmail.com>
2024-03-07 11:10:44 +0100

    Implementing chages to facilitate task completion

--- packages/excalidraw/renderer/renderElement.ts
+++ packages/excalidraw/renderer/renderElement.ts
@@ -334,19 +334,8 @@ const drawElementOnCanvas = (
     case "image": {
       const img = isInitializedImageElement(element)
         ? renderConfig.imageCache.get(element.fileId)?.image
-        : undefined;
+        : undefined;   
       if (img != null && !(img instanceof Promise)) {
-        if (element.roundness && context.roundRect) {
-          context.beginPath();
-          context.roundRect(
-            0,
-            0,
-            element.width,
-            element.height,
-            getCornerRadius(Math.min(element.width, element.height), element),
-          );
-          context.clip();
-        }
         context.drawImage(
           img,
           0 /* hardcoded for the selection box*/,
