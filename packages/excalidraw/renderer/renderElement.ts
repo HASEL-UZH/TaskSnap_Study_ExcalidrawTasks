725b2c7f8f5ec2b215edaad238e175a489b75950
JulianaGSouza <juulianags@gmail.com>
2024-03-22 16:40:26 +0100

    Deffining changes to Task: Support roundness for images

--- packages/excalidraw/renderer/renderElement.ts
+++ packages/excalidraw/renderer/renderElement.ts
@@ -26,7 +26,7 @@ import {
   RenderableElementsMap,
 } from "../scene/types";
 import { distance, getFontString, isRTL } from "../utils";
-import { getCornerRadiusBySin, isRightAngle } from "../math";
+import { isRightAngle } from "../math";
 import rough from "roughjs/bin/rough";
 import {
   AppState,
@@ -337,17 +337,14 @@ const drawElementOnCanvas = (
         ? renderConfig.imageCache.get(element.fileId)?.image
         : undefined;
       if (img != null && !(img instanceof Promise)) {
-        if (element.roundness && context.roundRect) {
-          context.beginPath();
-          context.roundRect(
-            0,
-            0,
-            element.width,
-            element.height,
-            getCornerRadiusBySin(element.width, element.height),
-          );
+        
+        //#TASK Support Roundness for Images
+        if (element.roundness) {
+          
+          
           context.clip();
         }
+
         context.drawImage(
           img,
           0 /* hardcoded for the selection box*/,
