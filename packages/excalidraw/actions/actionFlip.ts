40fd9c162d281fcce5878ef4b83d0174bd15f424
JulianaGSouza <juulianags@gmail.com>
2024-07-06 12:51:34 +0200

    Improve paste behaviors, creating separated text elements

--- packages/excalidraw/actions/actionFlip.ts
+++ packages/excalidraw/actions/actionFlip.ts
@@ -41,6 +41,7 @@ export const actionFlipHorizontal = register({
   contextItemLabel: "labels.flipHorizontal",
 });
 
+//TODO: Resolve the shortcut conflict for plain text paste
 export const actionFlipVertical = register({
   name: "flipVertical",
   trackEvent: { category: "element" },
